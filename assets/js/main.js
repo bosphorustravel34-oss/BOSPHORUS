b4be73f5c9d38996de6922087ac8942ea28b0b3b
bosphorustravel34-oss <bosphorustravel34@gmail.com>
2026-06-09 21:00:04 +0000

    add all sliders

--- assets/js/main.js
+++ assets/js/main.js
@@ -93,3 +93,93 @@ slider("vip-img", [
 "assets/images/vip-transfer/13.jpg",
 "assets/images/vip-transfer/14.jpg"
 ]);
+
+slider("yat-img", [
+"assets/images/yat/Y1.jpg",
+"assets/images/yat/Y2.jpg",
+"assets/images/yat/Y3.jpg",
+"assets/images/yat/Y4.jpg",
+"assets/images/yat/Y5.jpg",
+"assets/images/yat/Y6.jpg",
+"assets/images/yat/Y7.jpg",
+"assets/images/yat/Y8.jpg",
+"assets/images/yat/Y9.jpg",
+"assets/images/yat/Y10.jpg"
+]);
+
+slider("helikopter-img", [
+"assets/images/helikopter/H1.jpg",
+"assets/images/helikopter/H2.jpg",
+"assets/images/helikopter/H3.jpg",
+"assets/images/helikopter/H4.jpg",
+"assets/images/helikopter/H5.jpg",
+"assets/images/helikopter/H6.jpg",
+"assets/images/helikopter/H7.jpg",
+"assets/images/helikopter/H8.jpg",
+"assets/images/helikopter/H9.jpg",
+"assets/images/helikopter/H10.jpg"
+]);
+
+slider("mugla-img", [
+"assets/images/mugla/M1.jpg",
+"assets/images/mugla/M2.jpg",
+"assets/images/mugla/M3.jpg",
+"assets/images/mugla/M4.jpg",
+"assets/images/mugla/M5.jpg",
+"assets/images/mugla/M6.jpg",
+"assets/images/mugla/M7.jpg",
+"assets/images/mugla/M8.jpg",
+"assets/images/mugla/M9.jpg",
+"assets/images/mugla/M10.jpg"
+]);
+
+slider("pamukkale-img", [
+"assets/images/pamukkale/P1.jpg",
+"assets/images/pamukkale/P2.jpg",
+"assets/images/pamukkale/P3.jpg",
+"assets/images/pamukkale/P4.jpg",
+"assets/images/pamukkale/P5.jpg",
+"assets/images/pamukkale/P6.jpg",
+"assets/images/pamukkale/P7.jpg",
+"assets/images/pamukkale/P8.jpg",
+"assets/images/pamukkale/P9.jpg",
+"assets/images/pamukkale/P10.jpg"
+]);
+
+slider("efes-img", [
+"assets/images/efes/E1.jpg",
+"assets/images/efes/E2.jpg",
+"assets/images/efes/E3.jpg",
+"assets/images/efes/E4.jpg",
+"assets/images/efes/E5.jpg",
+"assets/images/efes/E6.jpg"
+]);
+
+slider("safari-img", [
+"assets/images/safari/S1.jpg",
+"assets/images/safari/S2.jpg",
+"assets/images/safari/S3.jpg",
+"assets/images/safari/S4.jpg",
+"assets/images/safari/S5.jpg",
+"assets/images/safari/S6.jpg",
+"assets/images/safari/S7.jpg"
+]);
+
+slider("diving-img", [
+"assets/images/diving/D1.jpg",
+"assets/images/diving/D2.jpg",
+"assets/images/diving/D3.jpg",
+"assets/images/diving/D4.jpg",
+"assets/images/diving/D5.jpg",
+"assets/images/diving/D6.jpg",
+"assets/images/diving/D7.jpg"
+]);
+
+slider("paragliding-img", [
+"assets/images/paragliding/Y1.jpg",
+"assets/images/paragliding/Y2.jpg",
+"assets/images/paragliding/Y3.jpg",
+"assets/images/paragliding/Y4.jpg",
+"assets/images/paragliding/Y5.jpg",
+"assets/images/paragliding/Y6.jpg"
+]);
